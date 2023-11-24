5cdefb6ad18f77b65f3e71e6ff1d38258a541faa
Bruno Mendonça <brunomendoncamal@gmail.com>
2023-11-24 11:07:20 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import axios from "axios"
+import FotoAmpliada from "./components/FotoAmpliada"
+import FotoList from "./components/FotoList"
+import Searchbar from "./components/Searchbar"
+
+import { useState, useEffect } from "react"
+
+function App() {
+  const [query, setQuery] = useState("")
+  const [category, setCategory] = useState("")
+  const [fotos, setFotos] = useState([])
+  const [fotoAmpliada, setFotoAmpliada] = useState(null)
+  const [activateSearch, setActivateSearch] = useState(false)
+
+  
+  const fechData = async ({query, category}) => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY
+
+    if(query || category) {
+      let searchQuery = query 
+
+      if(query && category) {
+        searchQuery = `${query} ${category}`
+      } else if (category) {
+        searchQuery = category
+      }
+
+      const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        client_id: apiKey,
+        query: searchQuery,
+      },
+    })
+    setFotos(response.data.results)
+    
+    return
+    }
+
+    const response = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: apiKey,
+        count: 10
+      },
+    })
+  setFotos(response.data)
+  }
+
+  useEffect(() => {
+    fechData(query, category)
+  }, [])
+
+  useEffect(() => {
+    
+    if(activateSearch) {
+      fechData({query, category})
+      setActivateSearch(false)
+    }
+
+  }, [activateSearch])
+
+  return (
+    <div className="container">
+      <Searchbar 
+        setQuery={setQuery} 
+        setCategory={setCategory} 
+        setActivateSearch={setActivateSearch}
+      />
+      <FotoList 
+        fotos={fotos} 
+        setFotoAmpliada={setFotoAmpliada}
+      />
+      {fotoAmpliada && (
+        <FotoAmpliada 
+          foto={fotoAmpliada}
+          setFotoAmpliada={setFotoAmpliada}
+        />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,37 @@
+
+const Searchbar = ({setQuery, setCategory, setActivateSearch}) => {
+  const categorias = [
+    "Natureza", 
+    "Pessoas", 
+    "Tecnologias", 
+    "Animais", 
+    "Esportes"
+  ]
+
+  return (
+    <div className="search-bar">
+      <input 
+        type="text" 
+        placeholder="Pesquisar fotos..." 
+        onChange={(e) =>  setQuery(e.target.value)}
+      />
+      <button onClick={() => setActivateSearch(true)}>Pesquisar</button>
+      <select onChange={(e) => {
+        setCategory(e.target.value)
+        setActivateSearch(true)
+      }}>
+          
+        {categorias.map((categoria) => (
+          <option 
+            key={categoria} 
+            value={categoria}
+          >
+            {categoria}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export default Searchbar
